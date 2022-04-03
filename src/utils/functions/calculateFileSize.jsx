85edb29b1e6ff60c99c99af6962264bb6d9104ee
Maseshi <dermhioasw123@gmail.com>
2022-04-03 20:38:55 +0700

    Added function for calculating file size.

---
+++ src/utils/functions/calculateFileSize.jsx
@@ -0,0 +1,16 @@
+export const calculateFileSize = (bytes, size = false, dp = 1) => {
+    const thresh = size ? 1000 : 1024
+
+    if (Math.abs(bytes) < thresh) return bytes + ' B'
+
+    let unit = -1
+    const units = size ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+    const r = (10 ** dp)
+
+    do {
+        bytes /= thresh
+        ++unit
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && unit < units.length - 1)
+
+    return bytes.toFixed(dp) + ' ' + units[unit]
+}
