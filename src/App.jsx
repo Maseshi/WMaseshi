944b8142b3a3ad5f17a6b8d3f1f533489274ae22
Maseshi <dermhioasw123@gmail.com>
2022-04-03 19:47:46 +0700

    Improved component usability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { initializeApp } from "firebase/app"
 import { getAnalytics } from "firebase/analytics"
 import { getPerformance } from "firebase/performance";
@@ -12,56 +11,43 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
-// Components
-import Navbar from './components/Navbar/index'
-import Footer from './components/Footer/index'
+// Configs
+import config from './configs/config'
 
-// Pages
-import PageNotFound from './containers/Client/404/index'
+// Layouts
+import Header from './layouts/Header/index'
+import Main from './layouts/Main/index'
+import Footer from './layouts/Footer/index'
 
-import Home from './pages/Home/index'
-import Account from './pages/Account/index'
-import ToS from './pages/Terms of Service/index'
-import PP from './pages/Privacy Policy/index'
+// Styles
+import './styles.css'
 
 export default function App() {
   const app = initializeApp({
-    "apiKey": process.env.REACT_APP_API_KEY,
-    "authDomain": process.env.REACT_APP_AUTH_DOMAIN,
-    "databaseURL": process.env.REACT_APP_DATABASE_URL,
-    "projectId": process.env.REACT_APP_PROJECT_ID,
-    "storageBucket": process.env.REACT_APP_STORAGE_BUCKET,
-    "messagingSenderId": process.env.REACT_APP_MESSAGING_SENDER_ID,
-    "appId": process.env.REACT_APP_APP_ID,
-    "measurementId": process.env.REACT_APP_MEASUREMENT_ID
+    "apiKey": config.server.API_KEY,
+    "authDomain": config.server.AUTH_DOMAIN,
+    "databaseURL": config.server.DATABASE_URL,
+    "projectId": config.server.PROJECT_ID,
+    "storageBucket": config.server.STORAGE_BUCKET,
+    "messagingSenderId": config.server.MESSAGING_SENDER_ID,
+    "appId": config.server.APP_ID,
+    "measurementId": config.server.MEASUREMENT_ID
   })
 
   if (process.env.NODE_ENV === 'production') {
     getAnalytics(app)
     getPerformance(app)
     initializeAppCheck(app, {
-      provider: new ReCaptchaV3Provider('6Lfe_f4aAAAAABG_DmZUtkHDmNfM7mf8Nhp9PYAz'),
+      provider: new ReCaptchaV3Provider(config.RECAPTCHA_V3_PROVIDER),
       isTokenAutoRefreshEnabled: true
     })
   }
 
   return (
     <BrowserRouter>
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <Routes>
-          <Route path="*" element={<PageNotFound />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/terms-of-service" element={<ToS />} />
-          <Route path="/privacy-policy" element={<PP />} />
-        </Routes>
-      </main>
-      <footer>
-        <Footer />
-      </footer>
+      <Header />
+      <Main />
+      <Footer />
     </BrowserRouter>
   )
 }
