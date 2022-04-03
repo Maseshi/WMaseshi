5e61af01d37f47d82fc7a0d1e2e8c3dd70608338
Maseshi <dermhioasw123@gmail.com>
2022-04-03 20:32:45 +0700

    Change the use of new props.

--- src/pages/Account/Profile.jsx
+++ src/pages/Account/Profile.jsx
@@ -1,17 +1,17 @@
 export default function Profile(props) {
-    const currentUser = props.currentUser
+    const userData = props.userData
 
     return (
-        <div className="account-profile">
+        <div className="account-profile mb-3">
             {
-                currentUser ? currentUser.photoURL ? (
-                    <img src={ currentUser.photoURL } alt="" width="100px" height="100px" />
+                userData ? userData.user.photoURL ? (
+                    <img src={ userData.user.photoURL } alt="" width="100px" height="100px" />
                 ) : (
                     <i className="bi bi-person-circle"></i>
                 ) : <i className="bi bi-person-circle"></i>
             }
-            <h3>{ currentUser ? currentUser.displayName || "ผู้ใช้" : "ผู้ใช้" }</h3>
-            <small>{ currentUser ? currentUser.email || "example@domain.com" : "example@domain.com" }</small>
+            <h3>{ userData ? userData.user.displayName || "ผู้ใช้" : "ผู้ใช้" }</h3>
+            <small>{ userData ? userData.user.email || "example@domain.com" : "example@domain.com" }</small>
         </div>
     )
 }
