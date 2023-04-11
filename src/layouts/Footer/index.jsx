32c82073152bcf40244b141d4730054c6150192e
Maseshi <dermhioasw123@gmail.com>
2023-04-11 23:15:04 +0700

    Update: Information data

--- src/layouts/Footer/index.jsx
+++ src/layouts/Footer/index.jsx
@@ -16,7 +16,7 @@ export default function Footer() {
                         <div className="row">
                             <div className="col-md-4 mb-3">
                                 <a className="footer-brand" href="/">
-                                    <img className="d-inline-block align-middle" src={process.env.PUBLIC_URL + '/static/media/favicon-96x96.png'} alt="favicon" width="50" height="50" />
+                                    <img className="d-inline-block align-middle" src={process.env.PUBLIC_URL + '/static/media/favicon-96x96.png'} alt="Maseshi" width="50" height="50" />
                                     Maseshi
                                 </a>
                                 <br />
@@ -28,6 +28,9 @@ export default function Footer() {
                                     {translator().translate.layouts.Footer.need_to_contact}
                                     <br />
                                     <a href="mailto:dermhioasw123@gmail.com">dermhioasw123@gmail.com</a>
+                                </p>
+                                <p>
+                                    {translator().translate.layouts.Footer.currently + (' v' + require('../../../package.json').version + ' ') + translator().translate.layouts.Footer.code_licensed} <a href="https://github.com/Maseshi/WMaseshi/blob/main/LICENSE" rel="noreferrer noopener" target="_blank">MIT</a>
                                 </p>
                             </div>
                             <div className="col-md-4 mb-3">
@@ -65,7 +68,7 @@ export default function Footer() {
                                 <br />
                                 <ul className="footer-link">
                                     <li className="footer-link-item">
-                                        <Link className="footer-link-a" to="./projects?id=wmaseshi" target="_blank" rel="noreferrer">
+                                        <Link className="footer-link-a" to="./projects?id=wmaseshi" target="_blank" rel="noreferrer noopener">
                                             {translator().translate.layouts.Footer.open_source}
                                         </Link>
                                     </li>
