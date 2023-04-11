f7cd1ce91fff92a857663b2e69eabbb7d882b217
Maseshi <dermhioasw123@gmail.com>
2023-04-11 23:38:38 +0700

    Update: Date format and header props.

--- src/pages/Projects/Pages/PageContents/ContentDetail.jsx
+++ src/pages/Projects/Pages/PageContents/ContentDetail.jsx
@@ -1,14 +1,12 @@
 import { isMobile } from "../../../../utils/functions/isMobile"
 import { translator } from "../../../../utils/functions/translator"
 
-export default function ContentDetail(props) {
-    const dataProps = props.data
-
+export default function ContentDetail({ data }) {
     const dateCreate = (established) => {
         if (!established) return translator().translate.pages.Projects.Pages.PageContents.ContentDetail.unknown
 
-        const date = new Date(established * 1000)
-        const day = date.getUTCDate()
+        const date = new Date((established.seconds || established) * 1000)
+        const day = date.getDate()
         const months = [
             translator().translate.pages.Projects.Pages.PageContents.ContentDetail.january,
             translator().translate.pages.Projects.Pages.PageContents.ContentDetail.february,
@@ -23,12 +21,11 @@ export default function ContentDetail(props) {
             translator().translate.pages.Projects.Pages.PageContents.ContentDetail.november,
             translator().translate.pages.Projects.Pages.PageContents.ContentDetail.december
         ]
-        const month = months[date.getUTCMonth()]
+        const month = months[date.getMonth()]
         const year = date.getFullYear() + 543
 
         return day + " " + month + " " + year
     }
-
     const statusStyle = (status) => {
         let style = ""
 
@@ -81,21 +78,19 @@ export default function ContentDetail(props) {
 
         return style
     }
-
     const externalLink = (link) => {
         let style = ''
 
         if (!link) return
 
         link.forEach((anchor) => {
-            style += '<a class="projects-content-link" href="' + anchor + '" target="_blank" rel="noreferrer">' +
+            style += '<a class="projects-content-link" href="' + anchor + '" target="_blank" rel="noreferrer noopener">' +
                 '<i class="bi bi-link-45deg"></i> ' + anchor +
                 '</a>'
         })
 
         return <p dangerouslySetInnerHTML={{ __html: style }}></p>
     }
-
     const tagCreator = (tags) => {
         let style = ''
 
@@ -107,7 +102,6 @@ export default function ContentDetail(props) {
 
         return <p dangerouslySetInnerHTML={{ __html: style }}></p>
     }
-
     const buttonCreate = (button) => {
         let style = ""
 
@@ -124,28 +118,28 @@ export default function ContentDetail(props) {
 
             switch (color) {
                 case 0:
-                    style += '<a class="projects-content-button btn btn-primary mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
+                    style += '<a class="btn btn-primary mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer noopener" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
                     break;
                 case 1:
-                    style += '<a class="projects-content-button btn btn-secondary mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
+                    style += '<a class="btn btn-secondary mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer noopener" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
                     break;
                 case 2:
-                    style += '<a class="projects-content-button btn btn-success mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
+                    style += '<a class="btn btn-success mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer noopener" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
                     break;
                 case 3:
-                    style += '<a class="projects-content-button btn btn-danger mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
+                    style += '<a class="btn btn-danger mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer noopener" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
                     break;
                 case 4:
-                    style += '<a class="projects-content-button btn btn-warning mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
+                    style += '<a class="btn btn-warning mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer noopener" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
                     break;
                 case 5:
-                    style += '<a class="projects-content-button btn btn-info mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
+                    style += '<a class="btn btn-info mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer noopener" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
                     break;
                 case 6:
-                    style += '<a class="projects-content-button btn btn-light mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
+                    style += '<a class="btn btn-light mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer noopener" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
                     break;
                 case 7:
-                    style += '<a class="projects-content-button btn btn-dark mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
+                    style += '<a class="btn btn-dark mb-3 ' + (disabled ? "disabled" : "") + '" href="' + link + '" target="_blank" rel="noreferrer noopener" role="button" aria-disabled="' + disabled + '">' + name + '</a>'
                     break;
                 default:
                     style += ''
@@ -160,8 +154,8 @@ export default function ContentDetail(props) {
             <div className="row">
                 <div className="col-md-2" align="center">
                     {
-                        dataProps.icon ? (
-                            <img className="projects-content-icon" src={dataProps.icon} width="100px" height="100px" alt={dataProps.id} />
+                        data.icon ? (
+                            <img className="projects-content-icon" src={data.icon} width="100px" height="100px" alt={data.id} />
                         ) : (
                             <div className="projects-content-no-icon">
                                 <span className="bi bi-archive"></span>
@@ -171,28 +165,28 @@ export default function ContentDetail(props) {
                 </div>
                 <div className="col-md-8" align="left">
                     <h3 className="projects-content-name">
-                        {dataProps.title}
+                        {data.title}
                     </h3>
                     {
                         isMobile() ? (
                             <>
-                                <p align="center">{dataProps.type}</p>
-                                <span>{translator().translate.pages.Projects.Pages.PageContents.ContentDetail.create_at}: {dateCreate(dataProps.established)}</span>
-                                <p>{translator().translate.pages.Projects.Pages.PageContents.ContentDetail.status}: {statusStyle(dataProps.status)}</p>
+                                <p align="center">{data.type}</p>
+                                <span>{translator().translate.pages.Projects.Pages.PageContents.ContentDetail.create_at}: {dateCreate(data.established)}</span>
+                                <p>{translator().translate.pages.Projects.Pages.PageContents.ContentDetail.status}: {statusStyle(data.status)}</p>
                             </>
                         ) : (
                             <p>
-                                <span>{dataProps.type} • </span>
-                                <span>{translator().translate.pages.Projects.Pages.PageContents.ContentDetail.create_at}: {dateCreate(dataProps.established)} • </span>
-                                <span>{translator().translate.pages.Projects.Pages.PageContents.ContentDetail.status}: {statusStyle(dataProps.status)}</span>
+                                <span>{data.type} • </span>
+                                <span>{translator().translate.pages.Projects.Pages.PageContents.ContentDetail.create_at}: {dateCreate(data.established)} • </span>
+                                <span>{translator().translate.pages.Projects.Pages.PageContents.ContentDetail.status}: {statusStyle(data.status)}</span>
                             </p>
                         )
                     }
-                    <p>{dataProps.description}</p>
-                    {externalLink(dataProps.link)}
-                    {tagCreator(dataProps.tag)}
+                    <p>{data.description}</p>
+                    {externalLink(data.link)}
+                    {tagCreator(data.tag)}
                 </div>
-                <div className="col-md-2" style={{ alignSelf: 'center' }} dangerouslySetInnerHTML={{ __html: buttonCreate(dataProps.button) }} align="center"></div>
+                <div className="col-md-2" style={{ alignSelf: 'center' }} dangerouslySetInnerHTML={{ __html: buttonCreate(data.button) }} align="center"></div>
             </div>
         </div>
     )
