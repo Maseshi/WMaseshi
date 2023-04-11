bd10285b0dbbe3aff2b9e91594751f63c7544a2a
Maseshi <dermhioasw123@gmail.com>
2023-04-11 23:34:51 +0700

    Update: Document meta and header props.

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -3,27 +3,25 @@ import { getFirestore, collection, onSnapshot } from 'firebase/firestore'
 import { getStorage, ref, getDownloadURL } from 'firebase/storage'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { getDatabase, ref as dbRef, child, get } from 'firebase/database'
-import DocumentMeta from 'react-document-meta'
+import { Helmet } from 'react-helmet'
 
 import ScrollToTop from '../../components/ScrollToTop'
 import CookieAccept from '../../components/CookieAccept'
 
 import Navbar from './Navbar'
-import Contents from './Pages/index'
+import Pages from './Pages/index'
 
 import { translator } from '../../utils/functions/translator'
 
 import './style.css'
 
 export default function Projects() {
-    const [data, setData] = useState([])
-    const [userData, setUserData] = useState()
-    const [loaded, setLoaded] = useState(false)
+    const [data, setData] = useState(false)
+    const [userData, setUserData] = useState(false)
 
     const url = new URL(window.location)
     const idParam = url.searchParams.get('id')
     const tabParam = url.searchParams.get('tab')
-
     const [parameter] = useState({
         id: idParam,
         tab: tabParam
@@ -37,7 +35,7 @@ export default function Projects() {
 
         onSnapshot(collection(db, "Projects"), querySnapshot => {
             let url = '', count = 0
-            const array = []
+            const queryArray = []
 
             querySnapshot.forEach(async doc => {
                 const id = doc.id
@@ -48,10 +46,9 @@ export default function Projects() {
                 const established = document.established
                 const icon = document.image.icon
                 const link = document.link
-                const isNew = document.new
                 const status = document.status
-                const tab = document.tab
-                const tag = document.tag
+                const tabs = document.tab
+                const tags = document.tag
                 const title = document.title
                 const type = document.type
 
@@ -64,69 +61,62 @@ export default function Projects() {
                     xhr.send()
                 }
 
-                array.push({
+                queryArray.push({
                     id: id,
                     background: background,
                     button: button,
                     description: description,
                     established: established,
                     icon: url,
+                    iconReference: icon,
                     link: link,
-                    new: isNew,
                     status: status,
-                    tab: tab,
-                    tag: tag,
+                    tab: tabs,
+                    tag: tags,
                     title: title,
                     type: type
                 })
 
                 ++count
 
-                if (count === querySnapshot.size) {
-                    setData(array)
+                if (count === querySnapshot.size) setData(queryArray)
+            })
+        })
 
-                    onAuthStateChanged(auth, async user => {
-                        if (user) {
-                            const uid = user.uid;
-                            const snapshot = await get(child(dbRef(database, 'projects/maseshi/users'), uid))
+        onAuthStateChanged(auth, async user => {
+            if (user) {
+                const uid = user.uid;
+                const snapshot = await get(child(dbRef(database, 'projects/maseshi/users'), uid))
 
-                            if (snapshot.exists()) {
-                                setUserData({
-                                    auth: user,
-                                    user: snapshot.val()
-                                })
-                                setLoaded(true)
-                            }
-                        } else {
-                            setUserData()
-                            setLoaded(true)
-                        }
+                if (snapshot.exists()) {
+                    setUserData({
+                        auth: user,
+                        user: snapshot.val()
                     })
                 }
-            })
+            } else {
+                setUserData()
+            }
         })
     }, [])
 
-    const meta = {
-        title: translator().translate.pages.Projects.Projects.website_title,
-        description: translator().translate.pages.Projects.Projects.website_description,
-        canonical: '/projects',
-        meta: {
-            name: {
-                keywords: 'maseshi, chaiwatsuwannarat, fluke, chaiwat',
-                subject: translator().translate.pages.Projects.Projects.subject,
-                language: 'TH',
-                robots: 'index, follow',
-
-                'og:type': 'website',
-                'og:image': '/maseshi_banner.jpg',
-                'og:site_name': 'Maseshi'
-            }
-        }
-    }
-
     return (
-        <DocumentMeta {...meta}>
+        <>
+            <Helmet>
+                <title>{translator().translate.pages.Projects.Projects.website_title}</title>
+                <meta name="description" content={translator().translate.pages.Projects.Projects.website_description} />
+                <meta name="keywords" content="maseshi, chaiwatsuwannarat, fluke, chaiwat" />
+                <meta name="subject" content={translator().translate.pages.Projects.Projects.subject} />
+                <meta name="language" content="TH" />
+                <meta name="robots" content="index, follow" />
+                <meta property="og:site_name" content="Maseshi" />
+                <meta property="og:title" content={translator().translate.pages.Projects.Projects.website_title} />
+                <meta property="og:description" content={translator().translate.pages.Projects.Projects.website_description} />
+                <meta property="og:image" content={process.env.PUBLIC_URL + '/maseshi_banner.jpg'} />
+                <meta property="og:url" content="https://maseshi.web.app/projects" />
+                <meta property="og:type" content="website" />
+                <link rel="canonical" href="https://maseshi.web.app/projects" />
+            </Helmet>
             <section className="projects">
                 <div className="navbar-expand-lg">
                     <button className="projects-navbar-button navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasProjectsNavbar" aria-controls="offcanvasProjectsNavbar">
@@ -136,16 +126,16 @@ export default function Projects() {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-md-3">
-                            <Navbar data={data.sort()} userData={userData} loaded={loaded} parameter={parameter} />
+                            <Navbar data={data ? data.sort() : ''} userData={userData} parameter={parameter} />
                         </div>
                         <div className="col-md-9">
-                            <Contents data={data.sort()} userData={userData} loaded={loaded} parameter={parameter} />
+                            <Pages data={data ? data.sort() : ''} userData={userData} parameter={parameter} />
                         </div>
                     </div>
                 </div>
                 <CookieAccept />
                 <ScrollToTop />
             </section>
-        </DocumentMeta>
+        </>
     )
 }
