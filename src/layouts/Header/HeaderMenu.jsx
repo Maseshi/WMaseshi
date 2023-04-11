54da461b042f84528c76f0b13b49c866489aafe9
Maseshi <dermhioasw123@gmail.com>
2023-04-11 23:16:19 +0700

    Change: Hashtag link

--- src/layouts/Header/HeaderMenu.jsx
+++ src/layouts/Header/HeaderMenu.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import { HashLink } from 'react-router-hash-link'
 
 import { translator } from '../../utils/functions/translator'
 
@@ -6,14 +7,14 @@ export default function HeaderMenu() {
     return (
         <ul className="navbar-nav me-auto mb-2 mb-lg-0 align-items-center">
             <li className="nav-item">
-                <Link className="nav-link" to="/#about">
+                <HashLink className="nav-link" to="/#about">
                     {translator().translate.layouts.Header.HeaderMenu.about}
-                </Link>
+                </HashLink>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/#skills">
+                <HashLink className="nav-link" to="/#skills">
                     {translator().translate.layouts.Header.HeaderMenu.ability}
-                </Link>
+                </HashLink>
             </li>
             <li className="nav-item">
                 <Link className="nav-link" to="/projects">
@@ -21,9 +22,9 @@ export default function HeaderMenu() {
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/#other">
+                <HashLink className="nav-link" to="/#other">
                     {translator().translate.layouts.Header.HeaderMenu.others}
-                </Link>
+                </HashLink>
             </li>
         </ul>
     )
