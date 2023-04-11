e2104ce3ee03667412d0b6ef035b7f302a8e3b24
Maseshi <dermhioasw123@gmail.com>
2023-04-11 23:45:21 +0700

    Update: Header props.

--- src/pages/Projects/Pages/PageEditor/NewsEditor/index.jsx
+++ src/pages/Projects/Pages/PageEditor/NewsEditor/index.jsx
@@ -1,18 +1,21 @@
 import { translator } from '../../../../../utils/functions/translator'
 
-export default function NewsEditor(props) {
-    const onSubmitProps = props.onSubmit
-    const onCancelProps = props.onCancel
-    const userDataProps = props.userData
+export default function NewsEditor({ onSubmit, onCancel, userData }) {
+    const handleDateCreate = (event) => {
+        event.target.classList.remove('is-invalid')
+    }
+    const handleTextareaCreate = (event) => {
+        if (event.target.value.length > event.target.minLength) event.target.classList.remove('is-invalid')
+    }
 
     return (
         <form className="projects-news-detail card mt-3 mb-3">
             <div className="row">
                 <div className="col-md-3" align="center">
-                    <img className="projects-news-author-avatar" src={userDataProps.auth.photoURL} width="80px" height="80px" alt="" />
+                    <img className="projects-news-author-avatar" src={userData.auth.photoURL} width="80px" height="80px" alt="" />
                     <br />
-                    <h3 className="projects-news-author-title">{userDataProps.auth.displayName}</h3>
-                    <span className="badge rounded-pill bg-primary">{userDataProps.user.role.toUpperCase()}</span>
+                    <h3 className="projects-news-author-title">{userData.auth.displayName}</h3>
+                    <span className="badge rounded-pill bg-primary">{userData.user.role.toUpperCase()}</span>
                 </div>
                 <div className="col-md-9" align="left">
                     <div className="card-body">
@@ -27,11 +30,7 @@ export default function NewsEditor(props) {
                                 aria-describedby="NewsCreateTextarea"
                                 id="NewsCreateTextarea"
                                 required
-                                onKeyUp={
-                                    (event) => {
-                                        event.target.classList.remove('is-invalid')
-                                    }
-                                }
+                                onKeyUp={(event) => handleDateCreate(event)}
                             />
                             <div id="NewsCreateTextarea" className="invalid-feedback">
                                 {translator().translate.pages.Projects.Pages.PageEditor.NewsEditor.date_is_invalid}
@@ -50,14 +49,10 @@ export default function NewsEditor(props) {
                                 maxLength="5000"
                                 id="NewsMessageTextarea"
                                 required
-                                onKeyUp={
-                                    (event) => {
-                                        if (event.target.value.length > event.target.minLength) event.target.classList.remove('is-invalid')
-                                    }
-                                }
+                                onKeyUp={(event) => handleTextareaCreate(event)}
                             ></textarea>
                             <small id="passwordHelpInline" className="text-muted">
-                                {translator().translate.pages.Projects.Pages.PageEditor.NewsEditor.content_support_markdown} <a href="https://www.markdownguide.org/" target="_blank" rel="noreferrer">https://www.markdownguide.org/</a>
+                                {translator().translate.pages.Projects.Pages.PageEditor.NewsEditor.content_support_markdown} <a href="https://www.markdownguide.org/" target="_blank" rel="noreferrer noopener">https://www.markdownguide.org/</a>
                             </small>
                             <div id="NewsMessageTextarea" className="invalid-feedback">
                                 {translator().translate.pages.Projects.Pages.PageEditor.NewsEditor.need_more_content}
@@ -65,10 +60,10 @@ export default function NewsEditor(props) {
                         </div>
                         <br />
                         <div className="projects-news-action d-grid gap-2 d-md-flex justify-content-md-center">
-                            <button className="btn btn-primary me-md-2" onClick={(event) => onSubmitProps(event)} type="button">
+                            <button className="btn btn-primary me-md-2" onClick={(event) => onSubmit(event)} type="button">
                                 {translator().translate.pages.Projects.Pages.PageEditor.NewsEditor.save}
                             </button>
-                            <button className="btn btn-secondary" onClick={(event) => onCancelProps(event)} type="button" id="NewsCancel">
+                            <button className="btn btn-secondary" onClick={(event) => onCancel(event)} type="button" id="NewsCancel">
                                 {translator().translate.pages.Projects.Pages.PageEditor.NewsEditor.cancel}
                             </button>
                         </div>
